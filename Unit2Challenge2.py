bd1a8a74695e14e40bd27cb916ab3235420e996a
asriyaesther <144555936+asriyaesther@users.noreply.github.com>
2023-10-16 19:52:00 +0530

    Create Unit2Challenge2.py

---
+++ Unit2Challenge2.py
@@ -0,0 +1,13 @@
+class Player():
+  def play(self):
+    print("The Player is playing.")
+class Batsman():
+    def play(self):
+      print("The Batsman is batting.")
+class Bowling():
+  def play(self):
+    print("The Bowler is bowling.")
+batsman=Batsman()
+bowler=Bowling()
+batsman.play()
+bowler.play()
